43b7d1e7a5a76cbb8e8d49bfdad9989c3f02c4da
ProDog <xiuboard@foxmail.com>
2019-02-21 20:49:29 +0800

    Support for parameter arrays

--- Neo.SmartContract.Framework/Services/Neo/NativeAsset.cs
+++ Neo.SmartContract.Framework/Services/Neo/NativeAsset.cs
@@ -11,8 +11,14 @@ namespace Neo.SmartContract.Framework.Services.Neo
         public static extern byte[] Call(string method, byte[] assetId);
 
         [Syscall("Zoro.NativeNEP5.Call")]
-        public static extern byte[] Call(string method, byte[] assetId, byte[] parameters);
+        public static extern byte[] Call(string method, byte[] assetId, byte[] parameter);
 
+        [Syscall("Zoro.NativeNEP5.Call")]
+        public static extern byte[] Call(string method, byte[] assetId, byte[] sender, byte[] to, BigInteger amount);
+
+        [Syscall("Zoro.NativeNEP5.Call")]
+        public static extern byte[] Call(string method, object[] parameters);
+        
         [Syscall("Zoro.NativeNEP5.GetTransferLog")]
         public static extern TransferLog GetTransferLog(byte[] assetId, byte[] txid);
 
